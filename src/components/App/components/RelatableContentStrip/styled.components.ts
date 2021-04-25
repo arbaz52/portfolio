cce76485b84408e44f213edac1bca159ad01d8f6
Arbaz Ajaz <arbaz5256@gmail.com>
2021-04-25 20:52:32 +0500

    feat: responsive relatable strip

--- src/components/App/components/RelatableContentStrip/styled.components.ts
+++ src/components/App/components/RelatableContentStrip/styled.components.ts
@@ -1,9 +1,14 @@
 import styled from "assets/themes";
+import breakpoints from "assets/themes/breakpoints";
 
 export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: 24px;
+
+  @media only screen and (max-width: ${breakpoints.md}) {
+    flex-direction: column;
+  }
 `;
 
 export const TitleWithNumber = styled.div`
